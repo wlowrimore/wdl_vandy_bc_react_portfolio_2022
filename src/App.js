0c3744abb67a6b5b3381f752ac5a2430acbf7ecd
William Lowrimore <92553989+wlowrimore@users.noreply.github.com>
2022-05-03 11:36:52 -0500

    adds About component structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Main from "./components/Main";
+// import Main from "./components/Main";
+import About from './components/About';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Navbar />
       </div>
       <div>
+        <About />
         <Main />
       </div>
       <div className="ftr-pos">
