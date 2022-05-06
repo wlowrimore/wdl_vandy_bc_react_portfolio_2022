37112d0f1fbe439cfc08650ca838ef30310c938c
William Lowrimore <92553989+wlowrimore@users.noreply.github.com>
2022-05-06 10:56:19 -0500

    adds styled contact section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ProjectWindow from "./components/ProjectWindow";
+import Contact from "./components/Contact";
 import About from "./components/About";
 
 export default function App() {
@@ -15,6 +16,7 @@ export default function App() {
       <main>
         <About />
         <ProjectWindow />
+        <Contact />
       </main>
 
       <footer className="ftr-pos">
---
+++ src/components/Contact.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import { FaArrowCircleRight } from "react-icons/fa";
+
+export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    // check if email is the proper format
+    if (!validateEmail(email)) {
+      setErrorMessage("Invalid email type!");
+      return;
+    }
+
+    console.log({ name, email, message });
+    alert(`Thank you for your input, ${name}`);
+
+    // clear values in input fields
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
+  return (
+    <section id="contact" className="container contact">
+      <h1>Contact me</h1>
+      <form id="contact-form">
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            className="form-control"
+            type="name"
+            defaultValue={name}
+            onChange={(e) => setName(e.target.value)}
+            name="name"
+            placeholder="Name"
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            className="form-control info"
+            type="email"
+            defaultValue={email}
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="message">Message:</label>
+          <textarea
+            className="form-control"
+            name="message"
+            defaultValue={message}
+            onChange={(e) => setMessage(e.target.value)}
+            rows="5"
+            placeholder="Message"
+          />
+        </div>
+        <button
+          className="btn shadow-none"
+          type="submit"
+          onClick={handleFormSubmit}
+        >
+          <FaArrowCircleRight className="icon" />
+        </button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
+    </section>
+  );
+}
